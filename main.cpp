5128d65419e276e705fc6cc7147e5b3870df2b11
John Masi <johnrmasi@gmail.com>
2025-10-26 00:13:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
-#include "mainView.h"
+\#include "mainView.h"
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <memory>
 
 // g++ main.cpp -o app -lsfml-graphics -lsfml-window -lsfml-system
 
 
 int main() {
 	HashMap<int, int> h_map(9); 
-	//h_map.insert(1,9);
-	HashView h_view{h_map};
 
-	MainView* m_view{ &h_view };
+	std::unique_ptr<MainView> m_view = std::make_unique<HashView>(h_map);
 
 	sf::RenderWindow window(sf::VideoMode(1280,1080), "Window");
 	sf::Event event;
 
-	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)){
-			h_view.remove();
-	}
-
-
 	while(window.isOpen()) {
+
+		// Switch views here 
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)){
+			
+		}
 
 		while(window.pollEvent(event)) {
 			if(event.type == sf::Event::Closed) {
